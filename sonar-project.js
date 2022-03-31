d3e806be20164bbc556959f3d20d749b17bb7513
ulysse-skpn <47350711+ulysse-skpn@users.noreply.github.com>
2022-03-31 21:51:06 +0200

    inclusion of ionic & angular app to sonar sources
    inclusion of ionic & angular app to sonar sources || update TODOs

--- sonar-project.js
+++ sonar-project.js
@@ -5,7 +5,7 @@ sonarqubeScanner(
         options : {
             'sonar.login':'admin',
             'sonar.password':'azerty',
-            'sonar.sources':  'src',
+            'sonar.sources':  'src,application/angular/src,application/ionic/src',
             // 'sonar.tests':  'tests',
             'sonar.inclusions'  :  '**', // Entry point of your code
             // 'sonar.test.inclusions':  'src/**/*.spec.js,src/**/*.spec.jsx,src/**/*.test.js,src/**/*.test.jsx',
