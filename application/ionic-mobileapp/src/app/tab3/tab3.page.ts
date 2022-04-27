fff131ea98b3b8b70dd85864bb52874c40c32542
ulysse-skpn <47350711+ulysse-skpn@users.noreply.github.com>
2022-04-27 15:15:58 +0200

    prep bookmark page (WIP)
    prep bookmark page (WIP)

--- application/ionic-mobileapp/src/app/tab3/tab3.page.ts
+++ application/ionic-mobileapp/src/app/tab3/tab3.page.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ModalController, ToastController } from '@ionic/angular';
+import { BookmarksService } from '../services/bookmarks/bookmarks.service';
+import { IngredientsService } from '../services/ingredients/ingredients.service';
+import { RecipesService } from '../services/recipes/recipes.service';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['./../../global.scss']
 })
-export class Tab3Page {
+export class Tab3Page implements OnInit {
 
-  constructor() {}
+  connected:boolean = false
+  selectTab:string = "all"
+  searchTerm:string = ""
+  recipeArraySize!:number
+  recipeList = []
+  ingredientArraySize!:number
+  ingredientList = []
+  pageIndex:number = 0
+  pageSize:number = 30
 
+
+  constructor(
+    private bookmarkService:BookmarksService,
+    private recipeService:RecipesService,
+    private ingredientService:IngredientsService,
+    private toastController:ToastController,
+    private modalController:ModalController
+  ) {}
+
+  ngOnInit(): void 
+  {
+    if( sessionStorage.getItem("access_token")) this.connected = true
+    //?
+  }
 }
