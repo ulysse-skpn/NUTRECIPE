0ab31d6afbfdec2da4cda86ed5448198aef226a2
Ulysse SEKPON <47350711+ulysse-skpn@users.noreply.github.com>
2022-06-04 21:02:11 +0200

    test 3

--- src/lib/config/database.ts
+++ src/lib/config/database.ts
@@ -26,81 +26,11 @@ export const database = new Sequelize({
     username:process.env.DB_USER,
     password:process.env.DB_PASSWORD,
     models: [__dirname + "/entity"],
-    timezone:timezone(),
-    define:
-    {
-        freezeTableName:true
-    }
+    timezone:timezone()
 })
 
 // Entities & Intermediate Table
 database.addModels([Ingredient , Recipe , User , BookmarkIngredient , BookmarkRecipe ])
 
 database.authenticate()
-    .then( async () => {
-        logger.info("Database connected...")
-
-        database.sync()
-            .then( async (res) => {
-
-                await database.query("show tables" , {type: sequelize.QueryTypes.SHOWTABLES})
-
-                await database.query("SELECT COUNT(ingredientId) as elem FROM ingredients" , { plain:true , raw:true} )
-                    .then( async (e:any) => {
- 
-                            
-                        if( e.elem === 0 || e.elem === 1 ) 
-                        {
-                            InitEntities.init_ingredient()
-
-                            const ingredientRepository:any = new IngredientRepository()
-                            const ingredientsList = fetchIngredients()
-
-                            const half = Math.ceil( ingredientsList.length / 2 )
-                            await ingredientRepository.bulkCreate( ingredientsList.slice(0,half) )
-                            await ingredientRepository.bulkCreate( ingredientsList.slice(-half) )
-                        }
-
-                        logger.info( 'ingredients table initialized' )
-                    })   
-
-                await database.query("SELECT COUNT(recipeId) as elem FROM recipes" , { plain:true , raw:true} )
-                    .then( async (e:any) => {
-
-
-                        if( e.elem === 0 || e.elem === 1 )
-                        {
-                            InitEntities.init_recipe()
-                            
-                            const recipeRepository:any = new RecipeRepository()
-                            const recipesList = fetchRecipes()
-
-                            await recipeRepository.bulkCreate( recipesList )
-                        }
-
-                        logger.info( 'recipes table initialized' )
-                    })      
-
-                await database.query("SELECT COUNT(userId) as elem FROM users" , { plain:true , raw:true} )
-                    .then( async (e:any) => {
-                        InitEntities.init_user()
-                        logger.info( 'users table initialized' )
-                    })       
-
-                await database.query("SELECT COUNT(id) as elem FROM bookmarkingredients" , { plain:true , raw:true} )
-                    .then( async (e:any) => {
-                        InitEntities.init_bookmark_ingredient()
-                        logger.info( 'bookmarkingredients table initialized' )
-                    })    
-                    
-                await database.query("SELECT COUNT(id) as elem FROM bookmarkrecipes" , { plain:true , raw:true} )
-                    .then( async (e:any) => {
-                        InitEntities.init_bookmark_recipe()
-                        logger.info( 'bookmarkrecipes table initialized' )
-                    })       
-
-            })          
-
-    } )
-    .catch( err => logger.info(`Error : ${err.toString()}`) )
 
