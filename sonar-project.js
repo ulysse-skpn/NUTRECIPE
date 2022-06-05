4d34653e41612d2d139f49989b0f71918e695689
ulysse-skpn <47350711+ulysse-skpn@users.noreply.github.com>
2022-06-06 00:06:31 +0200

    code coverage in sonarqube for ionic and angular

--- sonar-project.js
+++ sonar-project.js
@@ -5,12 +5,12 @@ sonarqubeScanner(
         options : {
             'sonar.login':'admin',
             'sonar.password':'azerty',
-            'sonar.sources':  'src,application/angular-webapp/src,application/ionic-mobileapp/src',
+            'sonar.sources':  'src,application/angular-webapp/src/app,application/ionic-mobileapp/src/app',
             'sonar.tests':  '',
             'sonar.inclusions'  :  '**', // Entry point of your code
             // 'sonar.test.inclusions':  'src/**/*.spec.js,src/**/*.spec.jsx,src/**/*.test.js,src/**/*.test.jsx',
-            'sonar.test.inclusions':  'src/**/*.spec.ts,application/angular-webapp/src/**/*.spec.ts,application/ionic-mobileapp/src/**/*.spec.ts',
-            'sonar.javascript.lcov.reportPaths':  'coverage/lcov.info',
+            'sonar.test.inclusions':  'src/**/*.spec.ts,application/angular-webapp/src/app/**/*.spec.ts,application/ionic-mobileapp/src/app/**/*.spec.ts',
+            'sonar.javascript.lcov.reportPaths':'coverage/lcov.info,application/angular-webapp/coverage/angular-webapp/lcov.info,application/ionic-mobileapp/coverage/ngv/lcov.info',
             // 'sonar.testExecutionReportPaths':  'coverage/test-reporter.xml'
         }
     }, () => {});
